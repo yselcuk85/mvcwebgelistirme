3e9215398636164334a5f964c6ad331d53525498
yselcuk85 <yselcuk85@gmail.com>
2020-12-07 19:35:57 +0300

    web
    webproje

---
+++ Webproje/Webproje/Controllers/TelefonController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webproje.Controllers
+{
+    public class TelefonController : Controller
+    {
+        // GET: Telefon
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Edit(int Id)
+		{
+
+            var Telefon = Models.TelefonVeri.VeriListe.Where(x => x.Id == Id).FirstOrDefault();
+            return View();
+
+		}
+        [HttpPost]
+        public ActionResult Edit(Models.TelefonVeri telefonVeri)
+		{
+            var Telefon = Models.TelefonVeri.VeriListe.FirstOrDefault(x => x.Id == telefonVeri.Id);
+            Telefon.Telefon = telefonVeri.Telefon;
+            Telefon.Model = telefonVeri.Model;
+            Telefon.Fiyat = telefonVeri.Fiyat;
+            return RedirectToAction("Listele");
+
+
+		}
+        public ActionResult Delete(int Id)
+		{
+            var Telefon = Models.TelefonVeri.VeriListe.Where(x => x.Id == Id).FirstOrDefault();
+            return View(Telefon);
+        }
+        [HttpPost]
+        public ActionResult Delete(Models.TelefonVeri telefonVeri)
+		{
+            var Telefon = Models.TelefonVeri.VeriListe.FirstOrDefault(x => x.Id == telefonVeri.Id);
+            Models.TelefonVeri.VeriListe.Remove(Telefon);
+            return RedirectToAction("Listele");
+        }
+        public ActionResult Listele()
+		{
+            return View(Models.TelefonVeri.VeriListe);
+		}
+        
+    }
+}
---
+++ Webproje/Webproje/Models/Telefon.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webproje.Models
+{
+
+    public class TelefonVeri
+    {
+        public int Id { get; set; }
+        public string Telefon { get; set; }
+        public string Model { get; set; }
+        public short Yıl { get; set; }
+        public long Fiyat { get; set; }
+
+        public static List<TelefonVeri> VeriListe = new List<TelefonVeri>
+    {
+        new TelefonVeri
+        {
+            Id=1,
+            Telefon="İphone",
+            Model="XS",
+            Yıl=2020,
+            Fiyat=9800,
+        },
+        new TelefonVeri
+        {
+            Id=2,
+            Telefon="Huawei",
+            Model="Mate 20 Lite",
+            Yıl=2018,
+            Fiyat=2800,
+
+
+        },
+        new TelefonVeri
+        {
+            Id=3,
+            Telefon="Samsung",
+            Model="S10",
+            Yıl=2019,
+            Fiyat=3500,
+
+
+        },
+        new TelefonVeri
+        {
+            Id=4,
+            Telefon="Xiomi",
+            Model="Note 8 Pro",
+            Yıl=2020,
+            Fiyat=4000,
+
+
+        },
+        new TelefonVeri
+        {
+            Id=5,
+            Telefon="İphone",
+            Model="XR",
+            Yıl=2020,
+            Fiyat=10000,
+
+
+        },
+        new TelefonVeri
+        {
+            Id=6,
+            Telefon="Sony",
+            Model="XZ Premium",
+            Yıl=2015,
+            Fiyat=2500,
+
+
+        },
+
+    };
+    }
+}
